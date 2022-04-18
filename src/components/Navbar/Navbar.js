84dd97a8095391bf54aed1b3fdb3fc035b49a006
daudi13 <davouma13@gmail.com>
2022-04-18 18:06:42 +0300

    install @emotions

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,13 +1,12 @@
 import React from 'react-dom';
-import mic from './images/mic.png';
-import fish from './images/fish.png';
+import './Navbar.css';
 
-const Navbar = () => (
-  <nav>
-    <img src={fish} alt="fish" />
+const NavBar = () => (
+  <nav className="nav">
+    <img src="./images/fish.png" alt="fish" />
     <h2 className="">title</h2>
-    <img src={mic} alt="mic" />
+    <img src="./images/mic.png" alt="mic" />
   </nav>
 );
 
-export default Navbar;
+export default NavBar;
