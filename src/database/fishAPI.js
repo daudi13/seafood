99aace3286017085456eda082e198d68752ae453
daudi13 <davouma13@gmail.com>
2022-04-19 21:06:01 +0300

    Add promise

--- src/database/fishAPI.js
+++ src/database/fishAPI.js
@@ -1,1 +1,15 @@
 const baseUrl = 'https://www.fishwatch.gov/api/species';
+
+export default async function FetchFishData() {
+  const response = await fetch(baseUrl);
+  if (response.status !== 200) {
+    throw new Error(
+      'wrong endpoint, check endpoint and try again',
+    );
+  } try {
+    const res = response.json();
+    return res;
+  } catch (error) {
+    throw new Error(error.message);
+  }
+}
