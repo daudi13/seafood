e548ec06039a6f2b27bbd6d99f82ec94740251e9
daudi13 <davouma13@gmail.com>
2022-04-19 12:14:01 +0300

    Add title

--- src/components/Pages/Homepage.js
+++ src/components/Pages/Homepage.js
@@ -83,7 +83,7 @@ const Homepage = () => {
         <img className="header-img" src="https://images.unsplash.com/photo-1544551763-77ef2d0cfc6c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80" alt="fishing boat" />
         <div className="image-cover" />
       </header>
-      <h3>All fish</h3>
+      <h4>All fish species</h4>
       <main className="main">
         {fishData.map((fish) => (
           <FishCard
