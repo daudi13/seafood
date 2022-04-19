e1bcc4fda39c70d62506ae8630283dd21eb6485e
daudi13 <davouma13@gmail.com>
2022-04-19 16:27:15 +0300

    Add detailsPage route

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import DetailsPage from './components/Pages/DetailsPage';
 import Homepage from './components/Pages/Homepage';
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Homepage />} />
+        <Route path="/detailsPage" element={<DetailsPage />} />
       </Routes>
     </BrowserRouter>
   );
