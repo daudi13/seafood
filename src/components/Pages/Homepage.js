50927a8c99c4b273b0005b671e9adbfa04fa9f78
daudi13 <davouma13@gmail.com>
2022-04-19 11:42:45 +0300

    Adjust homepage componet

--- src/components/Pages/Homepage.js
+++ src/components/Pages/Homepage.js
@@ -8,42 +8,42 @@ const Homepage = () => {
     {
       id: '1',
       name: 'tuna',
-      img: 'https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.bates.edu%2Fbiology%2Ffiles%2F2015%2F03%2Ftunaseinersdotcom.jpg&f=1&nofb=1',
+      img: 'https://www.fishwatch.gov/sites/default/files/Atlantic%20Chub%20Mackerel_Alessandro%20Duci.jpg',
     },
     {
       id: '2',
-      name: 'tuna',
-      img: 'https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.bates.edu%2Fbiology%2Ffiles%2F2015%2F03%2Ftunaseinersdotcom.jpg&f=1&nofb=1',
+      name: 'Lobster',
+      img: 'https://www.fishwatch.gov/sites/default/files/2_6.jpg',
     },
     {
       id: '3',
-      name: 'tuna',
-      img: 'https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.bates.edu%2Fbiology%2Ffiles%2F2015%2F03%2Ftunaseinersdotcom.jpg&f=1&nofb=1',
+      name: 'Rockfish',
+      img: 'https://www.fishwatch.gov/sites/default/files/Rockfish_Yellowtail_NB_W.png',
     },
     {
       id: '4',
       name: 'tuna',
-      img: 'https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.bates.edu%2Fbiology%2Ffiles%2F2015%2F03%2Ftunaseinersdotcom.jpg&f=1&nofb=1',
+      img: 'https://www.fishwatch.gov/sites/default/files/Bocaccio_NB_W.png',
     },
     {
       id: '5',
       name: 'tuna',
-      img: 'https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.bates.edu%2Fbiology%2Ffiles%2F2015%2F03%2Ftunaseinersdotcom.jpg&f=1&nofb=1',
+      img: 'https://www.fishwatch.gov/sites/default/files/Kelp_Sugar_NB_W.png',
     },
     {
       id: '6',
       name: 'tuna',
-      img: 'https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.bates.edu%2Fbiology%2Ffiles%2F2015%2F03%2Ftunaseinersdotcom.jpg&f=1&nofb=1',
+      img: 'https://www.fishwatch.gov/sites/default/files/rsz_1beer_battered_halibut_square_darren_howe_noaa_federal.png',
     },
     {
       id: '7',
       name: 'tuna',
-      img: 'https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.bates.edu%2Fbiology%2Ffiles%2F2015%2F03%2Ftunaseinersdotcom.jpg&f=1&nofb=1',
+      img: 'https://www.fishwatch.gov/sites/default/files/3%20-%20halibut2_fullsize.jpg',
     },
     {
       id: '8',
       name: 'tuna',
-      img: 'https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.bates.edu%2Fbiology%2Ffiles%2F2015%2F03%2Ftunaseinersdotcom.jpg&f=1&nofb=1',
+      img: 'https://www.fishwatch.gov/sites/default/files/1%20photo-west-coast-region-photo-gallery.jpg',
     },
     {
       id: '9',
