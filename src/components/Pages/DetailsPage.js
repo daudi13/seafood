c0443391925f9b2d36d805cedc4e65d2e871e45d
daudi13 <davouma13@gmail.com>
2022-04-19 20:42:43 +0300

    Add new accordion

--- src/components/Pages/DetailsPage.js
+++ src/components/Pages/DetailsPage.js
@@ -17,7 +17,7 @@ const DetailsPage = () => {
       <main>
         <Accordion defaultActiveKey="0" flush>
           <Accordion.Item eventKey="0">
-            <Accordion.Header className="accordion-header">Accordion Item #1</Accordion.Header>
+            <Accordion.Header className="accordion-eader" style={{ backgroundColor: 'pink' }}>Accordion Item #1</Accordion.Header>
             <Accordion.Body className="accordion">
               <p>
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
@@ -30,7 +30,7 @@ const DetailsPage = () => {
               </p>
             </Accordion.Body>
           </Accordion.Item>
-          <Accordion.Item eventKey="1" className="accordion-header">
+          <Accordion.Item eventKey="1">
             <Accordion.Header>Accordion Item #2</Accordion.Header>
             <Accordion.Body className="accordion">
               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
@@ -40,6 +40,20 @@ const DetailsPage = () => {
               velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
               cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
               est laborum.
+            </Accordion.Body>
+          </Accordion.Item>
+          <Accordion.Item eventKey="0">
+            <Accordion.Header className="accordion-eader" style={{ backgroundColor: 'pink' }}>Accordion Item #1</Accordion.Header>
+            <Accordion.Body className="accordion">
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+                veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+                commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+                velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+                cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+                est laborum.
+              </p>
             </Accordion.Body>
           </Accordion.Item>
         </Accordion>
