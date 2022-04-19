e3fc3d14d1cc3d60c136ddebb2503742a4746927
daudi13 <davouma13@gmail.com>
2022-04-19 17:48:07 +0300

    Add back conditional ternary

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,10 +1,11 @@
 import PropTypes from 'prop-types';
 import React from 'react-dom';
+import ArrowBackIosNewRoundedIcon from '@mui/icons-material/ArrowBackIosNewRounded';
 import './Navbar.css';
 
 const NavBar = ({ title, back }) => (
   <nav className="nav">
-    {back || <img className="nav-icons" src="./images/fish.png" alt="fish" />}
+    {back ? <ArrowBackIosNewRoundedIcon /> : <img className="nav-icons" src="./images/fish.png" alt="fish" />}
     <h2 className="nav-title">{title}</h2>
     <div className="nav-search">
       <img className="nav-icons" src="./images/mic.png" alt="mic" />
@@ -15,7 +16,11 @@ const NavBar = ({ title, back }) => (
 
 NavBar.propTypes = {
   title: PropTypes.string.isRequired,
-  back: PropTypes.bool.isRequired,
+  back: PropTypes.bool,
+};
+
+NavBar.defaultProps = {
+  back: false,
 };
 
 export default NavBar;
