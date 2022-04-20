833129a5f9f0cda5d6d667185d12d85dc9b78584
daudi13 <davouma13@gmail.com>
2022-04-20 07:58:57 +0300

    Add providers and store attribute

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
+import { Provider } from 'react-redux';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import DetailsPage from './components/Pages/DetailsPage';
 import Homepage from './components/Pages/Homepage';
+import store from './redux/configureStore';
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Homepage />} />
-        <Route path="/detailsPage" element={<DetailsPage />} />
-      </Routes>
+      <Provider store={store}>
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/detailsPage" element={<DetailsPage />} />
+        </Routes>
+      </Provider>
     </BrowserRouter>
   );
 }
