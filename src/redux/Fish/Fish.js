adacc851de13e786d2ffac167f404b1a1519ac02
daudi13 <davouma13@gmail.com>
2022-04-20 07:22:20 +0300

    Add action reducer functions

---
+++ src/redux/Fish/Fish.js
@@ -0,0 +1,52 @@
+import FetchFishData from '../../database/fishAPI';
+
+const GET_FISH_REQUEST = 'seafood/Fish/FETCH/GET_REQUEST';
+const GET_FISH_SUCCESS = 'seafood/Fish/GET_SUCCESS';
+const GET_FISH_FAIL = 'seafood/Fish/GET_FAIL';
+
+const initialState = {
+  loading: false,
+  fishArray: [],
+  error: '',
+};
+
+const getFishRequest = () => ({
+  type: GET_FISH_REQUEST,
+});
+
+const getFishSuccess = (fishArray) => ({
+  type: GET_FISH_SUCCESS,
+  payload: fishArray,
+});
+
+const getFishFail = (error) => ({
+  type: GET_FISH_FAIL,
+  payload: error,
+});
+
+export default function getFish() {
+  return (dispatch) => {
+    dispatch(getFishRequest());
+    FetchFishData()
+      .then((data) => {
+        const fishJSON = data.map((dat) => ({
+          sciName: dat['Scientific Name'],
+          normalName: dat['Species Name'],
+          biology: dat.Biology,
+          calories: dat.Calories,
+          carbohydrates: dat.Carbohydrate,
+          fat: dat['Fat, Total'],
+          proteins: dat.Protein,
+          imgArr: dat['Image Gallery'],
+          locale: dat.Location,
+          population: dat['Population Status'],
+          harvest: dat.Harvest,
+          health: dat['Health Benefits'],
+        }));
+        dispatch(getFishSuccess(fishJSON));
+      })
+      .catch((error) => {
+        dispatch(getFishFail(error.message));
+      });
+  };
+}
