5af41ae3034cc14acb7915acd807a0dc5ff3d8a8
daudi13 <davouma13@gmail.com>
2022-04-20 20:01:20 +0300

    new class to navbar element

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -9,7 +9,7 @@ const NavBar = ({ title, back }) => (
     {back ? (<NavLink to="/"><ArrowBackIosNewRoundedIcon /></NavLink>) : <img className="nav-icons" src="./images/fish.png" alt="fish" />}
     <h2 className="nav-title">{title}</h2>
     <div className="nav-search">
-      <img className="nav-icons" src="./images/mic.png" alt="mic" />
+      <img className="nav-icons mic" src="./images/mic.png" alt="mic" />
       <img className="nav-icons" src="./images/settings-cog.png" alt="mic" />
     </div>
   </nav>
