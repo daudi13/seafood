cf13a8437d870b8a14c18db22ce4807d688f29ff
David Ouma <davouma13@gmail.com>
2022-04-21 22:58:50 +0300

    remove store from App.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,16 @@
 /* eslint linebreak-style: ["error", "windows"] */
-import { Provider } from 'react-redux';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import DetailsPage from './components/Pages/DetailsPage';
 import Homepage from './components/Pages/Homepage';
-import store from './redux/configureStore';
 
 function App() {
   return (
     <BrowserRouter>
-      <Provider store={store}>
-        <Routes>
-          <Route path="/" element={<Homepage />} />
-          <Route path="/detailsPage" element={<DetailsPage />} />
-        </Routes>
-      </Provider>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/detailsPage" element={<DetailsPage />} />
+      </Routes>
     </BrowserRouter>
   );
 }
