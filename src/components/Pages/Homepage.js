47016e6cb8b9e4c44ba0197f72a42dce9ee40cc0
daudi13 <davouma13@gmail.com>
2022-04-22 15:18:28 +0300

    Add homepage header image

--- src/components/Pages/Homepage.js
+++ src/components/Pages/Homepage.js
@@ -17,9 +17,9 @@ const Homepage = () => {
 
   return (
     <>
-      <header>
-        <NavBar title="SaveEarth" />
-        <img className="header-img" src="https://images.unsplash.com/photo-1544551763-77ef2d0cfc6c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80" alt="fishing boat" />
+      <header className="header">
+        <NavBar title="PerspectiveWild" />
+        <img className="home-img" src="./images/kindpng_7177875.png" alt="im" />
         <div className="image-cover" />
       </header>
       <h4>Animal species</h4>
